cfc0e74564d9fdcd7acb033f6dbf6fe0284c60cd
Stuart <stu@alwaystwisted.com>
2013-02-19 10:32:27 +0000

    created a quick perch starting point, made includes for header/footer. Deleted stuff

---
+++ includes/footer.php
@@ -0,0 +1,22 @@
+	<footer role="contentinfo">
+
+	</footer> <!-- /footer -->
+
+</div> <!-- /#wrapper -->
+
+
+
+
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery.js"><\/script>')</script>
+
+<script src="js/plugins.js"></script>
+<script src="js/main.js"></script>
+
+  <script>
+    var _gaq=[['_setAccount','UA-XXXX-XX'],['_trackPageview']];(function(a,b){var c=a.createElement(b),d=a.getElementsByTagName(b)[0];c.src=("https:"==location.protocol?"//ssl":"//www")+".google-analytics.com/ga.js";d.parentNode.insertBefore(c,d)})(document,"script");
+  </script>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+// Allows <title> to be set per page.
+$page_title = "home";
+
+?>
+<?php include("perch/runtime.php"); ?>
+
+<?php include("includes/header.php"); ?>
+
+<?php include("includes/footer.php"); ?>
